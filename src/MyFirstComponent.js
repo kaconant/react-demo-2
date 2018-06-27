7c001d5c0d78915df4fb209e7a00eb3b001b46ca
kaconant <krissyconant@gmail.com>
2018-06-26 21:22:37 -0400

    added new files

---
+++ src/MyFirstComponent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import App from './App.js'
+
+let MyFirstComponent = () => {
+
+    let myNumber = 3;
+    for (let i= 0; i<10; i++) {
+        myNumber++;
+    }
+
+    return (
+    <div>
+        {myNumber}
+        <div>
+            <App />
+        </div>
+    </div>
+    )
+}
+
+export default MyFirstComponent;
